8cb1073753736855f05daea667466ed7aa518dfe
IlyaStudent <ilyaprepodavatel@gmail.com>
2025-07-28 21:06:10 +0300

    drift fix

--- data/lib/src/data_sources/local/database/database.dart
+++ data/lib/src/data_sources/local/database/database.dart
@@ -7,8 +7,10 @@ import 'package:data/data.dart';
 import 'package:domain/domain.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 // import 'package:drift_flutter/drift_flutter.dart';
 // import 'package:path_provider/path_provider.dart';
 
