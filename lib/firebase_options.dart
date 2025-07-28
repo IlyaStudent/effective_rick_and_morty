7610896d1a956fb6f78a3067e60fbe7a8c8434d8
IlyaStudent <ilyaprepodavatel@gmail.com>
2025-07-28 22:00:51 +0300

    test

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -22,7 +22,6 @@ class DefaultFirebaseOptions {
         'you can reconfigure this by running the FlutterFire CLI again.',
       );
     }
-
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
